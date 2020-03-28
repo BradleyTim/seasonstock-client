4982b75555e46fef11b22db09fe464bcd35632b7
BradleyTim <timbradley94@gmail.com>
2020-03-28 18:03:35 +0300

    new nuxtjs app

---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class=".about">
+    <h3 class="about--title">
+      About Us
+    </h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  head () {
+    return {
+      title: 'About Us',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'We are all about Premier League Jerseys and Kits'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.about {
+  width: 90%;
+  margin: 0 auto;
+}
+
+.about--title {
+  font-size: 50px;
+  font-weight: 300;
+}
+</style>
