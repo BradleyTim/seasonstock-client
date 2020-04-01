e180fddc2ad5b62d03f4907e2d365a29171baf17
BradleyTim <timbradley94@gmail.com>
2020-04-01 22:09:20 +0300

    initial admin pages

---
+++ pages/admin/add-jersey.vue
@@ -0,0 +1,96 @@
+<template>
+  <section class="add-jersey-page">
+    <div>ADD JERSEY</div>
+    <form enctype="multipart/form-data" v-if="!loading" @submit.prevent="addJersey">
+      <input v-model="jersey.name" type="text" name="name" placeholder="name">
+      <input type="file" name="image" placeholder="image" @change="onFileChange">
+      <input v-model="jersey.price" type="text" name="price" placeholder="price">
+      <input v-model="jersey.kit" type="text" name="kit" placeholder="kit">
+      <button type="submit" class="btn">
+        ADD
+      </button>
+    </form>
+    <app-loading v-if="loading" />
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+import AppLoading from '~/components/AppLoading.vue'
+
+export default {
+  components: {
+    AppLoading
+  },
+  data () {
+    return {
+      jersey: {
+        name: null,
+        image: null,
+        price: null,
+        kit: null
+      },
+      loading: false,
+      error: null
+    }
+  },
+  methods: {
+    onFileChange (e) {
+      const files = e.target.files || e.dataTransfer.files
+      if (files.length) {
+        this.jersey.image = files[0]
+      }
+    },
+    async addJersey () {
+      console.log('running')
+      const options = {
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'multipart/form-data'
+        }
+      }
+
+      const { name, image, price, kit } = this.jersey
+      const fd = new FormData()
+      fd.append('name', name)
+      fd.append('image', image)
+      fd.append('price', price)
+      fd.append('kit', kit)
+      const API_URL = 'https://jerseystore-api.now.sh/api/v1/jerseys'
+
+      this.loading = true
+
+      if (this.jersey.name !== '') {
+        try {
+          const response = await axios.post(API_URL, fd, options)
+          console.log(response)
+          if (response.status === 201) {
+            this.loading = false
+            this.jersey = {
+              name: null,
+              image: null,
+              price: null,
+              kit: null
+            }
+          }
+        } catch (error) {
+          console.log(error)
+          this.loading = false
+          this.error = true
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.add-jersey-page {
+  margin: 4rem 0;
+}
+
+input {
+  display: block;
+  margin: 1rem 0;
+}
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div>{{ title }}</div>
+    <nuxt-link class="btn" to="/admin/add-jersey">
+      New Jersey
+    </nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'ADMIN'
+    }
+  }
+}
+</script>
+
+<style scoped>
+section {
+  margin: 4rem 0;
+}
+
+div {
+  color: red;
+}
+
+.btn {
+  display: inline-block;
+  margin-top: 1rem;
+  border: 1px solid #eee;
+  padding: .25rem 1rem;
+}
+</style>
